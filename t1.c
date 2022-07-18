b5209e4b6461833f33c835a052ee9cd2c2226ff4
mjvini <40394180+mjvini@users.noreply.github.com>
2022-07-18 17:20:34 -0300

    Update t1.c

--- t1.c
+++ t1.c
@@ -3,29 +3,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+FILE *out ;
+
 typedef enum _TOKEN {
     eof = -1, identificador = 0, invalido = 1
 } TOKEN;
 
+int n_linhas = 1; //contador de linhas
+int n_coluna = 1; //contador de colunas
+
 %}
+
+NL \n
+RESTO .
 
 COM1 [^\n]*
 COM2 [^*/]* 
 PROT (if)|(else)|(while)|var|const|return|fn|=|bool|int|float|true|false|[(]|[)]|[{]|[}]|[;]|[:]
 ID  [a-zA-z]+ 
-LI  ([1-9][0-9]+)|([0])   
+LI  ([1-9][0-9]*)|([0])   
 LF  (([1-9][0-9]+)|([0]))[\.][0-9]+
 OB  [+]|[*]|==
+INVALIDO [.*]|([0][0]+[0-9]+[\.][0-9]+)|([0]+)|([\.][0-9]*)
 
 %%
-"//"{COM1}   {printf("%s é um Comentario\n", yytext);}
-"/*"{COM2}"*/"  {printf("%s é um Comentario\n", yytext);}
-{PROT}  {printf("%s é uma Palavra Reservada\n", yytext);}
-{ID}    {printf("%s é um Identificador\n", yytext);}
-{LI}    {printf("%s é um Literais inteiros\n", yytext);}
-{LF}    {printf("%s é um Literais Floats\n", yytext);}
-{OB}    {printf("%s é um Operadores Binarios\n", yytext);}      
-['end'] { return -1; }
+    //Regras:
+
+{INVALIDO}    {fprintf(out,"(Invalido,\"%s\",%d,%d)\n",yytext,n_linhas, n_coluna);}
+"//"{COM1}   {fprintf(out,"(Comentario,\"%s\",%d,%d)\n",yytext,n_linhas, n_coluna);}
+"/*"{COM2}"*/"  {fprintf(out,"(Comentario,\"%s\",%d,%d)\n",yytext,n_linhas, n_coluna);}
+{PROT}  {fprintf(out,"(Palavra Reservada,\"%s\",%d,%d)\n",yytext,n_linhas, n_coluna);}
+{ID}    {fprintf(out,"(Identificador,\"%s\",%d,%d)\n",yytext,n_linhas, n_coluna);}
+{LI}   	{fprintf(out,"(Literal Inteiro,\"%s\",%d,%d)\n",yytext,n_linhas, n_coluna);}
+{LF}    {fprintf(out,"(Literal float,\"%s\",%d,%d)\n",yytext,n_linhas, n_coluna);}
+{OB}    {fprintf(out,"(Operador Binario,\"%s\",%d,%d)\n",yytext,n_linhas, n_coluna);} 
+{NL} {n_linhas++, n_coluna = 1;}
+{RESTO} {n_coluna++;}
 
 %%
 
@@ -35,12 +48,12 @@ int yywrap(){
     return 1; 
 }
 
-int main()
-{
-    char fname[100];
-    printf("\nEnter the name of file\n");
-    scanf("%s",fname);
-    yyin = fopen(fname,"r+");
+int main(int argc, char *argv[]){
+
+    FILE *arquivo = fopen(argv[1],"r");
+    yyin = arquivo;
+    
+    out = fopen(argv[2],"w");
     yylex();
     return 0;
 }
